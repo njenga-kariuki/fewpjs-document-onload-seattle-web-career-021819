a8da091da00f6198aad6b8c1bbc07f258f0f7676
njenga <njengak@me.com>
2019-04-01 17:00:09 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const id_text = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", function() {
+  id_text.innerHTML = 'This is really cool!'
+});
+
+
+console.log('hi')
